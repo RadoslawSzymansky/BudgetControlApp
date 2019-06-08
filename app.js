1cd28c770e44d4ae0c90dce6445f2cd2fe488658
Radoslaw Szymanski <radek199@gmail.com>
2019-06-08 13:42:16 +0200

    aaA

--- app.js
+++ app.js
@@ -33,12 +33,23 @@ app.use('/users', usersRouter);
 app.use('/api', apiRouter)
 
 // nadpisane
-app.use(express.static(path.join(__dirname, 'client/build')));
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => { res.sendfile(path.join(__dirname = 'client/build/index.html')); })
-}
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('*', (req, res) => { res.sendfile(path.join(__dirname = 'client/build/index.html')); })
+// }
 // app.get('*', (req, res) => { res.sendFile(path.join(__dirname + '/client/public/index.html')); })
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 
 module.exports = app;
