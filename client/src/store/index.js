1a05e60b6af3d57f638d29aceabb1db170e4a95c
Radoslaw Szymanski <radek199@gmail.com>
2019-06-08 15:16:37 +0200

    comment line to redux devtools in store/ index'

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,7 +7,7 @@ const initialState = {};
 
 const store = createStore(reducer, initialState, compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
