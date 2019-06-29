9d2ca892ba25d512d6aea2f3251cae78a8d89514
Radoslaw Szymanski <radek199@gmail.com>
2019-06-29 11:31:28 +0200

    disable in production redux dev tools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,10 +4,18 @@ import reducer from '../reducers/index';
 
 const middleware = [thunk];
 const initialState = {};
+if (process.env.NODE_ENV === 'production') { 
+
+}
+
+
 
 const store = createStore(reducer, initialState, compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  process.env.NODE_ENV  === 'production' 
+    ? 
+    null : 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
